d7c787ce7c4847b94550549618591ad935076b16
n-c-ui <macmos639@gmail.com>
2021-10-19 18:54:35 +0300

    adding a recipe detail page

---
+++ lib/recipe_detail.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/recipe.dart';
+
+class RecipeDetail extends StatefulWidget {
+  final Recipe recipe;
+
+  const RecipeDetail({
+    Key? key,
+    required this.recipe,
+  }) : super(key: key);
+
+  @override
+  _RecipeDetailState createState() {
+    return _RecipeDetailState();
+  }
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  @override
+  Widget build(BuildContext context) {}
+}
